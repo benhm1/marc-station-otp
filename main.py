ec4b10a671dc1f487a0ccac977f4146e2a857cd0
Heroku Backend <heroku-backend@localhost.localdomain>
2025-10-11 13:48:42 -0400

    Add marc otp code

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#  Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 import train_data
 import git_helpers
 
